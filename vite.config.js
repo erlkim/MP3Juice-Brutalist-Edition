8e2e3f929da798e79867491843ddd3514d9c35ab
Erlkim <99765959+erlkim@users.noreply.github.com>
2026-05-03 02:28:33 +0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Konfigurasi dasar Vite untuk aplikasi React
+export default defineConfig({
+  plugins: [react()],
+})
